f78783a8fc4e5e9a307499cb7bd00e7999141469
neco3s <neco3coffee@gmail.com>
2023-08-26 06:36:39 +0900

    pathに応じて現在activeなnavの色を目立たせる

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,31 +1,47 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+
   return (
     <ul className="text-sm w-full fixed z-20 bottom-0 left-0 font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow flex dark:divide-gray-700 dark:text-gray-400">
-      <li className="w-full">
+      <li className="w-full border-none">
         <Link
           href="/"
           aria-current="page"
-          className="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
+          className={`${
+            pathname === "/"
+              ? "bg-gray-600 text-gray-100"
+              : "bg-gray-100 text-gray-900"
+          } inline-block w-full p-4 rounded-lg`}
         >
           Home
         </Link>
       </li>
-      <li className="w-full">
+      <li className="w-full border-none">
         <Link
           href="/link"
           aria-current="page"
-          className="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
+          className={`${
+            pathname === "/link"
+              ? "bg-gray-600 text-gray-100"
+              : "bg-gray-100 text-gray-900"
+          } inline-block w-full p-4 rounded-lg`}
         >
           Link
         </Link>
       </li>
-      <li className="w-full">
+      <li className="w-full border-none">
         <Link
           href="/profile"
           aria-current="page"
-          className="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
+          className={`${
+            pathname === "/profile"
+              ? "bg-gray-600 text-gray-100"
+              : "bg-gray-100 text-gray-900"
+          } inline-block w-full p-4 rounded-l-lg`}
         >
           Profile
         </Link>
